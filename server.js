8fd933b6bb17e3a00922243cf53b24f0847ff6f6
eogiesoba <eogiesoba@gmail.com>
2018-03-24 13:33:09 -0500

    Updated the server.js file to connect to mongoDB using heroku

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(routes);
 mongoose.Promise = global.Promise;
 // Connect to the Mongo DB
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist",
+  process.env.MONGODB_URI || "mongodb://user:password@ds123499.mlab.com:23499/heroku_jxl9860v",
   {
     useMongoClient: true
   }
