9ca31bfb78c9c056c0cd9eab21cc5b30dfda4c4a
eogiesoba <eogiesoba@gmail.com>
2018-03-29 22:26:19 -0500

    Added password authentication to Rools App

--- config/passport.js
+++ config/passport.js
@@ -16,18 +16,18 @@ passport.use(new LocalStrategy(
     db.User.findOne({ username: username}
     ).then(function(dbUser) {
       // If there's no user with the given username
-    //   if (!dbUser) {
-    //     return done(null, false, {
-    //       message: "Incorrect username."
-    //     });
-    //   }
-    //   // If there is a user with the given username, but the password the user gives us is incorrect
-    //   else if (!dbUser.validPassword(password)) {
-    //     return done(null, false, {
-    //       message: "Incorrect password."
-    //     });
-    //   }
-    //   // If none of the above, return the user
+      if (!dbUser) {
+        return done(null, false, {
+          message: "Incorrect username."
+        });
+      }
+      // If there is a user with the given username, but the password the user gives us is incorrect
+      else if (!dbUser.comparePassword(password)) {
+        return done(null, false, {
+          message: "Incorrect password."
+        });
+      }
+      // If none of the above, return the user
         console.log(dbUser);
       return done(null, dbUser);
     });
